e83fc632c148986feab84ca569a68dc7b6080cb5
Patric <patric@patriccampbell.com>
2017-11-28 13:45:47 -0500

    Add generateAuthToken function to User model

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const { sign, verify } = require("jsonwebtoken");
 
-const User = mongoose.model("User", {
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -25,5 +26,22 @@ const User = mongoose.model("User", {
     },
   ],
 });
+
+UserSchema.methods.generateAuthToken = function() {
+  const user = this;
+  const access = "auth";
+  const token = sign(
+    { _id: user.id.toHexString(), access },
+    "patriciscool",
+  ).toString();
+
+  user.tokens.push({ access, token });
+
+  return user.save().then(() => {
+    return token;
+  });
+};
+
+const User = mongoose.model("User", UserSchema);
 
 module.exports = { User };
